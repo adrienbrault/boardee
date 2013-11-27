693bd01ee85fa82984e5b664ee7733d8ca5f933e
Adrien Brault <adrien.brault@gmail.com>
2013-11-27 10:16:31 -0800

    Remove yellow/gold line colors

--- client/views/graph/image.js
+++ client/views/graph/image.js
@@ -2,10 +2,26 @@ Template.graphImage.helpers({
     url: function () {
         return graphUrl(this, {
             format: 'svg',
+
+            // white background template
             bgcolor: 'white',
             fgcolor: 'black',
             majorGridLineColor: 'gray',
-            minorGridLineColor: 'gray'
+            minorGridLineColor: 'gray',
+
+            // remove the yellow/gold colors that are not cool on white background
+            colorList: [
+                'blue',
+                'green',
+                'red',
+                'purple',
+                'brown',
+                'aqua',
+                'grey',
+                'magenta',
+                'pink',
+                'rose'
+            ].join(',')
         });
     }
 });
