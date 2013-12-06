afaa8941a8f7fbefe24e2ff5cabff6bf09046fdb
Adrien Brault <adrien.brault@gmail.com>
2013-12-06 02:04:30 -0800

    graphs are a bit nicer

--- client/views/graph/image.js
+++ client/views/graph/image.js
@@ -1,6 +1,6 @@
 var isRetinaDevice = window.devicePixelRatio >= 2;
 
-var scale = 1.3;
+var scale = 1;
 var height = 250 * scale;
 var width = 330 * scale;
 
@@ -14,13 +14,15 @@ Template.graphImage.helpers({
             width: width,
             height: height,
 
+            fontSize: 8,
+
             // white background template
             bgcolor: 'white',
             fgcolor: 'black',
             majorGridLineColor: 'gray',
             minorGridLineColor: 'gray',
 
-            lineWidth: isRetinaDevice ? 0.5 : 1,
+            lineWidth: isRetinaDevice ? 0.75 : 1,
 
             // remove the yellow/gold colors that are not cool on white background
             colorList: [
